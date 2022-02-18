4983a8a8a6dcdb991d4bcf3f7a47973643405a17
Vaakom <systemplast@yandex.ru>
2022-02-18 23:59:04 +0300

    Let's go

---
+++ src/main/java/ore/scyllacore/ScyllaCoreApplication.java
@@ -0,0 +1,13 @@
+package ore.scyllacore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScyllaCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScyllaCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ore/scyllacore/ScyllaCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ore.scyllacore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScyllaCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
