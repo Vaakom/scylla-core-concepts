35eed01e48b45442707d83ef0066d7fb271dc4c2
Vaakom <systemplast@yandex.ru>
2022-02-23 21:35:00 +0300

    First step, connection test

---
+++ src/test/java/ore/scyllacore/repositories/HeartrateRepositoryTest.java
@@ -0,0 +1,20 @@
+package ore.scyllacore.repositories;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeartrateRepositoryTest {
+
+    @Before
+    void setUp() {
+
+    }
+
+    @Test
+    public void doTmg() {
+        System.out.println("Test Zero");
+    }
+}
